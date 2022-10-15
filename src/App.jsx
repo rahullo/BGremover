abd44eb8d658ac8f2b304efcd1328ce3f18b880f
Rahul Lohra <rahullohra987@gmail.com>
2022-10-15 23:36:11 +0530

    Adjusting requirement

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return ( 
+        <div className = "App" >
+            <h5 className='high'> 🙈🙉🙊🐵🦄🦍🐈🐕🐩🐕‍🦺🦌🦬🦏🐎🐫</h5>
+        </div>
+    );
+}
+
+export default App;
